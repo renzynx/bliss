af9c5eecea0637ffc1e01c03e07d6b5421fa55f8
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-20 22:26:24 +0700

    better admin setup

--- server/src/libs/setup.ts
+++ server/src/libs/setup.ts
@@ -2,30 +2,44 @@ import { PrismaClient } from "@prisma/client";
 import * as argon from "argon2";
 import { randomString } from "./functions";
 import logger from "./logger";
+import readline from "readline";
 
 const setup = async () => {
 	const client = new PrismaClient();
+
 	logger.info("Connecting to MYSQL...");
 
 	const count = await client.user.count();
 
 	if (count > 0) return logger.error("You already run this setup script, you don't need to run it again");
 
-	logger.info("Creating admin user...");
-
-	const plainPassword = randomString();
-	const hashedPassword = await argon.hash(plainPassword);
-
-	await client.user.create({
-		data: {
-			username: "admin",
-			password: hashedPassword,
-			token: randomString(),
-			is_admin: true
-		}
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
 	});
 
-	return logger.info(`Created user with username: "admin" and password: "${plainPassword}"`);
+	return rl.question("What do you want your admin username to be: ", (username) => {
+		rl.question("What do you want your admin password to be: ", async (password) => {
+			const hash = await argon.hash(password);
+			await client.user
+				.create({
+					data: {
+						username,
+						password: hash,
+						token: randomString(),
+						is_admin: true
+					}
+				})
+				.then((user) => {
+					logger.info(`Created admin user "${user.username}"`);
+					process.exit(0);
+				})
+				.catch((err) => {
+					logger.error((err as Error).message);
+					process.exit(1);
+				});
+		});
+	});
 };
 
 setup()
