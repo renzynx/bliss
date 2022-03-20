485f62ad24b2d5abc470338bee448a97be110f4a
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-20 23:17:28 +0700

    more info on startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -34,6 +34,7 @@ const start = async () => {
 
 	__secure__ && logger.info("Secure mode enabled");
 	__prod__ && logger.info("Production mode enabled");
+	__prod__ && logger.info(`Frontend URL: ${process.env.FRONTEND_URL}`);
 	app.set("trust proxy", 1);
 	app.use(cors({ credentials: true, origin: __cors__ }));
 	app.use(
