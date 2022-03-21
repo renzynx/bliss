70a789582d33f49c28ecbf3ccc3936fbfbea11fc
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-21 11:50:33 +0700

    Yep, minimal was right on this one.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,14 +3,17 @@ import Head from 'next/head';
 import Navbar from '@layouts/Navbar';
 import withApollo from '@utils/withApollo';
 import HomePage from '@components/pages/HomePage';
+import { useMeQuery } from '@generated/graphql';
 
 const Home: NextPage = () => {
+	const { data, loading } = useMeQuery();
+
 	return (
 		<>
 			<Head>
 				<title>Home</title>
 			</Head>
-			<Navbar />
+			<Navbar data={data} loading={loading} />
 			<HomePage />
 		</>
 	);
